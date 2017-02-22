8f27288636fc5890b84bfbaa0a3fb91fb5724875
Liam Considine <liam.considine@adtegrity.com>
2017-02-22 09:33:52 -0500

    Through chapter 6 rendering HTML components

--- Bingo.elm
+++ Bingo.elm
@@ -1,20 +1,46 @@
 module Bingo exposing (..)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 playerInfo name gameNumber =
     name ++ " - Game #" ++ (toString gameNumber)
 
 
-playerInfoText name gameNumber =
-    playerInfo name gameNumber
-        |> String.toUpper
-        |> Html.text
+viewPlayer name gameNumber =
+    let
+        playerInfoText =
+            playerInfo name gameNumber
+                |> String.toUpper
+                |> text
+    in
+        h2 [ id "info", class "classy" ]
+            [ playerInfoText ]
+
+
+viewHeader title =
+    header []
+        [ h1 [] [ text title ] ]
+
+
+viewFooter =
+    footer []
+        [ a [ href "http://elm-lang.org" ]
+            [ text "Powered By Elm" ]
+        ]
+
+
+view =
+    div [ class "content" ]
+        [ viewHeader "BUZZWORD BINGO"
+        , viewPlayer "Nicole" 6
+        , viewFooter
+        ]
 
 
 main =
-    playerInfoText "Liam" 55
+    view
 
 
 
